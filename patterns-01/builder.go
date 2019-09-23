37475ae77af2d753b8c6753941ae363b4995da36
phpli <lichangdong100@gmail.com>
2019-09-23 19:04:36 +0800

    设计思想

---
+++ patterns-01/builder.go
@@ -0,0 +1,65 @@
+package main
+
+/*
+	separate the construction of a complex object from representation
+	设计思想：
+		*Builder interface (包含1.biuld_XX method 返回的是biulder接口，2.get_XX 返回对象)
+		*父struct
+		*Director struct, 属性为Builder, 实现Construct()和SetBuilder()方法
+		*不同的子struct组合，实现接口builder
+*/
+
+type wheel int
+//定义父类struct
+type Vehicle struct {
+		Wheels		wheel
+		Seats		int
+		Structure	string
+}
+
+type Builder interface {
+		SetWheels()		Builder
+		SetSeats()		Builder
+		SetStructure()	Builder
+		GetVehicle()	Vehicle
+}
+
+type Director struct {
+		builder		Builder
+}
+
+func (director *Director)Construct()  {
+		director.builder.SetWheels().SetSeats().SetStructure() ////链式调用
+}
+
+func (director *Director)SetBuilder(builder Builder)  {
+		director.builder = builder
+}
+
+type Car struct {
+		vehicle Vehicle
+}
+
+//实现继承Builder
+
+func (car *Car)SetWheels()Builder {
+		car.vehicle.Wheels = 4
+		return car
+}
+
+func (car *Car)SetSeats()Builder{
+		car.vehicle.Seats = 4
+		return car
+}
+
+func (car *Car)SetStructure()Builder  {
+		car.vehicle.Structure = "Car"
+		return car
+}
+
+func (car *Car)GetVehicle()Vehicle  {
+		return car.vehicle
+}
+
+
+
